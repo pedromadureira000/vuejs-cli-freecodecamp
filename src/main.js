0fe6b02aab6cff2a78e7d5b952e752d020463a79
Pedro Madureira <dev@phsolucoesweb.com.br>
2021-10-09 09:45:57 -0300

    finish.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// the vue CLI sets webpack in someway that you 
+//can import styles direcly in js, and it will work.
+import './assets/styles/style.scss'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
